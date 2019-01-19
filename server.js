24bdfc7971f0250dd8765fd2ce48a9f31daf950b
David He <davidyhe1194@gmail.com>
2019-01-19 17:06:58 -0500

    Included "Hello from" as requested.

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('David, student ID = 300844568, wants to buy a Switch, but realizes he would fail all his classes if he did.\n');
+    res.end('Hello from David, student ID = 300844568, whom wants to buy a Switch, but realizes he would fail all his classes if he did.\n');
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
+
