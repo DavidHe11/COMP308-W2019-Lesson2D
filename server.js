acdb29d1cbb39e27af3d0b54f265cdea7d7b3f11
David He <davidyhe1194@gmail.com>
2019-01-19 16:54:10 -0500

    initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('David, student ID = 300844568, wants to buy a Switch, but realizes he would fail all his classes if he did.\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
